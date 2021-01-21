6ee841e4cbf87b43cd932b80f7b2dc5a7c399019
alexvqXD1 <alexvq1003@hotmail.com>
2021-01-21 18:13:46 -0500

    conectando a la base de datos

--- signin.php
+++ signin.php
@@ -9,7 +9,7 @@
     {
         $un_temp = mysql_entities_fix_string($conexion, $_POST['user']);
         $pw_temp = mysql_entities_fix_string($conexion, $_POST['contra']);
-        $query   = "SELECT * FROM usuarios WHERE username='$un_temp'";
+        $query   = "SELECT * FROM usuario WHERE idusuario='$un_temp'";
         $result  = $conexion->query($query);
         
         if (!$result) die ("Usuario no encontrado");
@@ -18,19 +18,17 @@
             $row = $result->fetch_array(MYSQLI_NUM);
             $result->close();
 
-            if (password_verify($pw_temp, $row[3])) 
+            if (password_verify($pw_temp, $row[1])) 
             {
                 session_start();
                 $_SESSION['nombre']=$row[0];
-                $_SESSION['apellido']=$row[1];
-                echo htmlspecialchars("$row[0] $row[1]:
-                    hola $row[0], has ingresado como '$row[2]'");
+                echo htmlspecialchars("Bienvenido '$row[0]' tu rol es '$row[2]'" );
               
               echo <<<_END
               <pre>
               <form action='ndiario.php' method='post'>
               <input type='hidden' name='user' value='$un_temp'>
-              <input type='submit' value='nueva nota'>
+              <input type='submit' value='Asistencia'>
               </form>
               _END;
 
@@ -38,7 +36,7 @@
               <pre>
               <form action='vdiario.php' method='post'>
               <input type='hidden' name='user' value='$un_temp'>
-              <input type='submit' value='ver notas'>
+              <input type='submit' value='Matricularse'>
               </form>
               _END;
               
@@ -47,12 +45,12 @@
               Click para salir</a></p>");
             }
             else {
-                echo "Usuario/password incorrecto <p><a href='signup.php'>
+                echo "Usuario/password incorrecto <p><a href='registrarse.html'>
             Registrarse</a></p>";
             }
         }
         else {
-          echo "Usuario/password incorrecto <p><a href='signup.php'>
+          echo "Usuario/password incorrecto <p><a href='registrarse.html'>
       Registrarse</a></p>";
       }
 
